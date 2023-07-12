5d3934492c6fd0355a8366d6871951b3506577a7
5kdk <5kdk.code@gmail.com>
2023-07-13 05:09:29 +0900

    refactor: Post 컴포넌트로 분리

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,10 +1,8 @@
 import { useContext, useEffect } from 'react';
 import styled from 'styled-components';
-import ReactMarkdown from 'react-markdown';
-import remarkGfm from 'remark-gfm';
 import { useParams } from 'react-router-dom';
 import { IssuesContext } from '../context/Issues';
-import { List, Spinner } from '../components';
+import { List, Post, Spinner } from '../components';
 
 const Flex = styled.div`
   display: flex;
@@ -15,11 +13,6 @@ const Avatar = styled.img`
   height: 84px;
   border-radius: 50%;
   padding: 10px;
-`;
-
-const MarkDown = styled(ReactMarkdown)`
-  margin-left: 74px;
-  padding: 20px 10px;
 `;
 
 const Detail = () => {
@@ -44,7 +37,7 @@ const Detail = () => {
         <Avatar src={issue.user.avatar_url} alt="avartar image" />
         <List issue={issue} />
       </Flex>
-      <MarkDown remarkPlugins={[remarkGfm]}>{issue.body}</MarkDown>
+      <Post>{issue.body}</Post>
     </>
   );
 };
