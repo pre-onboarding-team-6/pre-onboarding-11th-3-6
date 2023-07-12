a020578d6966780c02f89f61b0bc1c27313e9158
5kdk <5kdk.code@gmail.com>
2023-07-12 19:47:21 +0900

    fix: api header 오류 수정

--- src/api/github.js
+++ src/api/github.js
@@ -5,7 +5,9 @@ const baseURL = `${API_URL.github}/repos/${API_URL.organization}/${API_URL.repos
 
 const githubAxios = axios.create({
   baseURL,
-  Authorization: `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`,
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`,
+  },
 });
 
 const getIssue = async () => {
