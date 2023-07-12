b434da3868cf6de4db76b05d1ddffc834fa5fcb9
5kdk <5kdk.code@gmail.com>
2023-07-12 17:20:09 +0900

    feat: main page UI 1차구현

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,5 +1,28 @@
 import { createGlobalStyle } from 'styled-components';
 
-const GlobalStyle = createGlobalStyle``;
+const GlobalStyle = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  ul, li {
+    list-style: none;
+  }
+
+  h2{
+    font-size: 16px;
+  }
+
+  span{
+    font-size: 14px;
+  }
+
+  a{
+    cursor: pointer;
+  }
+
+`;
 
 export default GlobalStyle;
