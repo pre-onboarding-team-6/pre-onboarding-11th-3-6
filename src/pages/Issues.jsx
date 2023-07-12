e420c272a76ddc07420024d1ddff331d72d4c3c3
YUNH7 <yunny126@gmail.com>
2023-07-12 23:43:33 +0900

    feat: Issues 무한스크롤 구현

--- src/pages/Issues.jsx
+++ src/pages/Issues.jsx
@@ -1,3 +1,35 @@
-const Issues = () => <main>Issues</main>;
+import { useState, useCallback, useRef, useEffect } from 'react';
+import IssuesBox from '../components/issues/IssuesBox';
+import { Loading, Error } from '../components/common';
+import { getIssues } from '../apis/issue';
+import useGetIssues from '../hooks/useGetIssues';
+
+const Issues = () => {
+  const [page, setPage] = useState(1);
+  const getFunc = useCallback(repo => getIssues(page)(repo), [page]);
+  const [issues, isLoading, error] = useGetIssues(getFunc);
+  const bottom = useRef();
+
+  useEffect(() => {
+    if (bottom.current && !isLoading) {
+      const observer = new IntersectionObserver(([entry], observer) => {
+        if (entry.isIntersecting) {
+          observer.unobserve(entry.target);
+          setPage(page => page + 1);
+        }
+      });
+      observer.observe(bottom.current);
+    }
+  }, [bottom, isLoading]);
+
+  return (
+    <main>
+      {issues[0] && <IssuesBox issues={issues} />}
+      {isLoading && <Loading />}
+      {error && <Error />}
+      <div ref={bottom} />
+    </main>
+  );
+};
 
 export default Issues;
