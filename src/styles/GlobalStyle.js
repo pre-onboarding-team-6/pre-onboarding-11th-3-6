efb4538cd21447814ca8566bc1cceaef860d47b1
YUNH7 <yunny126@gmail.com>
2023-07-12 21:46:17 +0900

    design: 기본 너비 480px 설정

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -5,6 +5,19 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
     margin: 0;
     box-sizing: border-box;
+    font-size: 16px;
+    word-break: keep-all;
+  }
+
+  ul, li {
+    list-style: none;
+  }
+
+  #root {
+    position: absolute;
+    width: 480px;
+    left: 50%;
+    transform: translateX(-50%);
   }
 `;
 
