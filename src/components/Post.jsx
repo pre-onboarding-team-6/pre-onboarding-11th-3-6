8c2265a57f334ee94ddb925552c6bb4a8db1cbae
YUNH7 <yunny126@gmail.com>
2023-07-13 23:45:44 +0900

    refactor: markdown 스타일 플러그인 추가, 이미지 너비 지정
    Co-authored-by: Dongkyu Kim <5kdk@users.noreply.github.com>

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,12 +1,51 @@
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import styled from 'styled-components';
+import rehypeRaw from 'rehype-raw';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { vs } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 const MarkDown = styled(ReactMarkdown)`
   margin-left: 74px;
   padding: 20px 10px;
+
+  & pre {
+    text-wrap: wrap;
+  }
+
+  & img,
+  code {
+    max-width: 100%;
+  }
 `;
 
-const Post = ({ children }) => <MarkDown remarkPlugins={[remarkGfm]}>{children}</MarkDown>;
+const CodeBlock = styled.code`
+  background-color: rgb(246, 248, 250);
+  border-radius: 6px;
+  padding: 0.2em 0.4em;
+  font-size: 85%;
+`;
+
+const Post = ({ children }) => (
+  <MarkDown
+    remarkPlugins={[remarkGfm]}
+    rehypePlugins={[rehypeRaw]}
+    components={{
+      code({ node, inline, className, children, ...props }) {
+        const match = /language-(\w+)/.exec(className || '');
+        return !inline && match ? (
+          <SyntaxHighlighter style={vs} language={match[1]} PreTag="div">
+            {String(children).replace(/\n$/, '')}
+          </SyntaxHighlighter>
+        ) : (
+          <CodeBlock className={className} {...props}>
+            {children}
+          </CodeBlock>
+        );
+      },
+    }}>
+    {children}
+  </MarkDown>
+);
 
 export default Post;
